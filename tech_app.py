55be19e0c28a434d0a92d19992b62721826348a9
O`rinboyev Barkamol <97020905+barkamoljon@users.noreply.github.com>
2023-01-28 00:10:01 +0500

    Update tech_app.py

--- tech_app.py
+++ tech_app.py
@@ -3,22 +3,6 @@ import pathlib
 import plotly.express as px
 import platform
 from fastai.vision.all import *
-import streamlit as st
-
-# Everything is accessible via the st.secrets dict:
-
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
-
-# And the root-level secrets are also accessible as environment variables:
-
-import os
-
-st.write(
-    "Has environment variables been set:",
-    os.environ["db_username"] == st.secrets["db_username"],
-)
 
 plt = platform.system()
 if plt == 'Linux' : pathlib.WindowsPath = pathlib.PosixPath
